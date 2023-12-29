1578e1f4c7bb17eeaa2318809e18d646121ba179
Logan Scott <123471880+LazyCiao@users.noreply.github.com>
2023-12-29 21:15:59 +0100

    Update views.py

--- dictionary/views.py
+++ dictionary/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 from .api_calls import fetch_definition
 
 def search(request):
-    api_key = "3f237fea22msh646ab05d0b6341cp135065jsnff5be19a03b7"  # Replace with your actual API key
+    api_key = ""  # Replace with your actual API key
     word = "test"  # Replace with the word you want to look up
     definition = fetch_definition(word, api_key)
     print(type(definition))
